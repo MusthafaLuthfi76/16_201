dad91b95ff45a07cf68d065752554057d2f2ad4c
MusthafaLuthfi76 <musthafa.luthfi76@gmail.com>
2025-01-22 16:03:22 +0700

    Menambahkan model Kategori.kt

---
+++ app/src/main/java/com/example/final_pam/model/Kategori.kt
@@ -0,0 +1,27 @@
+package com.example.final_pam.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Kategori(
+    @SerialName("id_kategori")
+    val idKategori: String,
+
+    @SerialName("Nama_kategori")
+    val namaKategori: String
+)
+
+@Serializable
+data class AllKategoriResponse(
+    val status: Boolean,
+    val message: String,
+    val data: List<Kategori>
+)
+
+@Serializable
+data class KategoriDetailResponse(
+    val status: Boolean,
+    val message: String,
+    val data: Kategori
+)
