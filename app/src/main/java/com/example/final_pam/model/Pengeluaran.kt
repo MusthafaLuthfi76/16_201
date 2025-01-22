6892cc1a82eb48a919c058aa54541e5d0ba1713f
MusthafaLuthfi76 <musthafa.luthfi76@gmail.com>
2025-01-22 16:09:07 +0700

    Menambahkan model Pengeluaran.kt

---
+++ app/src/main/java/com/example/final_pam/model/Pengeluaran.kt
@@ -0,0 +1,39 @@
+package com.example.final_pam.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Pengeluaran(
+    @SerialName("Id_pengeluaran")
+    val idPengeluaran: String,
+
+    @SerialName("Id_aset")
+    val idAset: String,
+
+    @SerialName("Id_kategori")
+    val idKategori: String,
+
+    @SerialName("Tanggal_transaksi")
+    val tglTransaksi: String,
+
+    @SerialName("total")
+    val total: String,
+
+    @SerialName("catatan")
+    val catatan: String,
+)
+
+@Serializable
+data class AllPendapatanResponse(
+    val status: Boolean,
+    val message: String,
+    val data: List<Aset>
+)
+
+@Serializable
+data class PendapatanDetailResponse(
+    val status: Boolean,
+    val message: String,
+    val data: Aset
+)
